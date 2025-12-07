0f40ddadc74655304011c5f0e747ffffbdbf4f7e
namyunju <skadbsnwk@naver.com>
2025-12-07 14:47:17 +0900

    Title: 요일 맞추기, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 251207/요일 맞추기/guess-day-of-week.java
+++ 251207/요일 맞추기/guess-day-of-week.java
@@ -1,58 +1,40 @@
 import java.util.Scanner;
+
 public class Main {
+    static final String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+    static final int[] num_days = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
     public static void solve(int m1, int d1, int m2, int d2) {
-        int day = 0;
-        String[] days = new String[] {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        int[] num_days = new int[] {0,31,28,31,30,31,30,31,31,30,31,30,31};
-        String ans;
+        int diff = 0;
+
         if (m1 == m2) {
-            if (d1 >= d2) {
-                day = (d1-d2)%7;
-                if (day == 0) {
-                    ans = days[day];
-                    System.out.print(ans);
-                } else {
-                    ans = days[7-day];
-                    System.out.print(ans);
-                }
-                return;
-
-            } else {
-                day = (d2-d1)%7;
-                ans = days[day];
-                System.out.print(ans);
-                return;
-            }
+            diff = d2 - d1;
         } else if (m1 < m2) {
-            day += num_days[m1] - d1;
-            for (int i=m1+1; i<m2-1; i++) {
-                day += num_days[i];
-            }
-            day += d2;
-        } else {
-            day += d1;
-            for (int i=m1-1; i>m2; i--) {
-                day += num_days[i];
-            }
-            day += num_days[m2]-d2;
-        }
-        day %= 7;
-        if (day == 0) {
-        ans = days[day];
-
-        } else {
-            ans = days[7-day];
+            diff += num_days[m1] - d1;        // 첫 달 남은 날짜
+            for (int i = m1 + 1; i < m2; i++) // 중간 달
+                diff += num_days[i];
+            diff += d2;                       // 마지막 달 날짜
+        } else { // m1 > m2
+            diff -= d1;                        // 첫 달 지나간 날짜
+            for (int i = m1 - 1; i > m2; i--)  // 중간 달
+                diff -= num_days[i];
+            diff -= (num_days[m2] - d2);       // 마지막 달 남은 날짜
         }
 
-        System.out.print(ans);
+        // 요일은 7로 나눠 정규화
+        int idx = ((diff % 7) + 7) % 7;
+
+        System.out.print(days[idx]);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        // Please write your code here.
-        solve(m1,d1,m2,d2);
+
+        solve(m1, d1, m2, d2);
+        sc.close();
     }
 }
