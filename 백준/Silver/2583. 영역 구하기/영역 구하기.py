6067aa434cf0d9494d50cc9348920c5c4ab8a34f
nam yunju <skadbsnwk@naver.com>
2026-03-05 20:32:28 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 114148 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,46 +1,43 @@
 import sys
+input = sys.stdin.readline
 from collections import deque
 
-def solve():
-    input = sys.stdin.read().split()
+M,N,K = map(int, input().split())
+# M행 N열
+board = [[0]*N for _ in range(M)]
+# 0 2 4 4 입력되면
+# 0과 4는 열을, 2와 4는 행을
+# 0열부터 3열까지
+# 아래에서부터 2,3 . 위에서부터 세면 1,2 즉 M-1-3부터 M-1-2
 
-    M, N, K = map(int, input[:3])
-    rects = input[3:]
+for _ in range(K):
+    c1, r1, c2, r2 = map(int, input().split())
+    for r in range(M-r2, M-r1):
+        for c in range(c1,c2):
+            board[r][c] = 1
 
-    grid = [[0] * N for _ in range(M)]
+ans = []
+def solve(i,j):
+    q = deque([(i,j)])
+    region = 1
+    
+    while q:
+        x,y = q.popleft()
+        board[x][y] = 1
+        for dx, dy in (0,1), (0,-1), (1,0), (-1,0):
+            nx, ny = x+dx, y+dy
+            if 0<= nx < M and 0 <= ny < N:
+                if board[nx][ny] == 0:
+                    q.append([nx,ny])
+                    board[nx][ny] = 1
+                    region += 1
+    ans.append(region)
 
-    idx = 0
-    for _ in range(K):
-        x1, y1, x2, y2 = map(int, rects[idx:idx+4])
-        for i in range(y1, y2):
-            for j in range(x1, x2):
-                grid[i][j] = 1
-        idx += 4
+for i in range(M):
+    for j in range(N):
+        if board[i][j] == 0:
+            solve(i,j)
 
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
-
-    areas = []
-
-    for i in range(M):
-        for j in range(N):
-            if grid[i][j] == 0:  
-                queue = deque([(i, j)])
-                grid[i][j] = 1  
-                count = 1
-                
-                while queue:
-                    y, x = queue.popleft()
-                    for d in range(4):
-                        ny, nx = y + dy[d], x + dx[d]
-                        if 0 <= ny < M and 0 <= nx < N and grid[ny][nx] == 0:
-                            grid[ny][nx] = 1
-                            count += 1
-                            queue.append((ny, nx))
-                
-                areas.append(count)
-
-    print(len(areas))
-    print(*(sorted(areas)))
-
-solve()
+ans.sort()
+print(len(ans))
+print(*ans)
