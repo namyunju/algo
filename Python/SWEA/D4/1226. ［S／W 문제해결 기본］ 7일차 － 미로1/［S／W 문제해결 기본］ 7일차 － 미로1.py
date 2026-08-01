fead61c98dc6d8c1246e8e454a23ecae1f8fdaf0
nam yunju <skadbsnwk@naver.com>
2026-08-01 13:40:17 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 92 ms, Memory: 60,800 KB -BaekjoonHub

---
+++ Python/SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,44 @@
+'''
+출발점에서 도착지점까지 가는 길 존재 여부
+'''
+def dfs(x, y):
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+
+    stack = [(x, y)]
+
+    while stack:
+        cx, cy = stack.pop()
+
+        for i in range(4):
+            nx, ny = cx + dx[i], cy + dy[i]
+
+            if (0 <= nx < 16) and (0 <= ny < 16):
+
+                if board[nx][ny] == 3:
+                    return 1
+                if board[nx][ny] == 0:
+                    board[nx][ny] = 1
+                    stack.append((nx, ny))
+    return 0
+
+    
+for _ in range(1, 11):
+    tc = int(input())
+    board = [list(map(int, input())) for i in range(16)]
+
+    sx = sy = 0
+    is_find = False
+    for i in range(16):
+        for j in range(16):
+            if board[i][j] == 2:
+                sx = i
+                sy = j
+                is_find = True
+                break
+        if is_find:
+            break
+                
+    ans = dfs(sx, sy)
+    print(f'#{tc} {ans}')
+    
